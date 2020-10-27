1b31c8973f0ad21a928a070196ebd24c234d50fa
Andrew <andrew.li729@gmail.com>
2020-10-27 16:56:23 -0500

    fixed variable name in class constructor

--- Lab_10.17.py
+++ Lab_10.17.py
@@ -2,10 +2,10 @@
 # 1824794
 
 class ItemToPurchase:
-    def __init__(self, name="none", item_price=0, item_quantity=0):
-        self.name = str(name)
-        self.item_price = float(item_price)
-        self.item_quantity = int(item_quantity)
+    def __init__(self, item_name="none", item_price=0, item_quantity=0):
+        self.name = item_name
+        self.item_price = item_price
+        self.item_quantity = item_quantity
 
     def print_item_cost(self):
         print("{} {} @ ${:.0f} = ${:.0f}".format(self.name, self.item_quantity, self.item_price, self.item_price * self.item_quantity))
