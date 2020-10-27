00953907d8ed9c4437a7d8d15b0ff6f032d7f429
Andrew <andrew.li729@gmail.com>
2020-10-27 15:41:17 -0500

    created Team class

--- Lab_10.15.py
+++ Lab_10.15.py
@@ -1,2 +1,12 @@
 # Andrew Li
 # 1824794
+class Team:
+    def __init__(self):
+        self.teamname = 'none'
+        self.teamwins = 0
+        self.team_losses = 0
+
+    def get_win_percentage(self):
+        return self.teamwins / (self.teamwins + self.team_losses)
+
+
