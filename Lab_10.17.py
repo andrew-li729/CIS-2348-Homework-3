451032a9b2ff0ca1805c91e92789dd95987e69e2
Andrew <andrew.li729@gmail.com>
2020-10-27 16:14:01 -0500

    created method for item cost

--- Lab_10.17.py
+++ Lab_10.17.py
@@ -2,8 +2,15 @@
 # 1824794
 
 class ItemToPurchase:
-    def __init__(self, name = "none", item_price=0, item_quantity=0):
+    def __init__(self, name="none", item_price=0, item_quantity=0):
         self.name = name
         self.item_price = item_price
         self.item_quantity = item_quantity
 
+    def print_item_cost(self):
+        total = self.item_price * self.item_quantity
+        print(total)
+
+
+item1 = ItemToPurchase()
+item1.print_item_cost()
