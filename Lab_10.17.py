6bd7f68f20d7347f1882bd7014b51259f697eb8c
Andrew <andrew.li729@gmail.com>
2020-10-27 16:46:41 -0500

    added types to default constructor variables

--- Lab_10.17.py
+++ Lab_10.17.py
@@ -3,9 +3,9 @@
 
 class ItemToPurchase:
     def __init__(self, name="none", item_price=0, item_quantity=0):
-        self.name = name
-        self.item_price = item_price
-        self.item_quantity = item_quantity
+        self.name = str(name)
+        self.item_price = float(item_price)
+        self.item_quantity = int(item_quantity)
 
     def print_item_cost(self):
         print("{} {} @ ${:.0f} = ${:.0f}".format(self.name, self.item_quantity, self.item_price, self.item_price * self.item_quantity))
@@ -21,12 +21,14 @@ if __name__ == '__main__':
     item1_quantity = int(input("Enter the item quantity:\n"))
     item1 = ItemToPurchase(item1_name, item1_price, item1_quantity)
     print()
+
     print("Item 2")
     item2_name = input("Enter the item name:\n")
     item2_price = float(input("Enter the item price:\n"))
     item2_quantity = int(input("Enter the item quantity:\n"))
     item2 = ItemToPurchase(item2_name, item2_price, item2_quantity)
     print()
+
     print("TOTAL COST")
     item1.print_item_cost()
     item2.print_item_cost()
