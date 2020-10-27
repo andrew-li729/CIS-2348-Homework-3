85cafbe356ccf259a1b339dc00124e91d1ec22ca
Andrew <andrew.li729@gmail.com>
2020-10-27 16:57:51 -0500

    changed itemname to item_name in class constructor

--- Lab_10.17.py
+++ Lab_10.17.py
@@ -3,12 +3,12 @@
 
 class ItemToPurchase:
     def __init__(self, item_name="none", item_price=0, item_quantity=0):
-        self.name = item_name
+        self.item_name = item_name
         self.item_price = item_price
         self.item_quantity = item_quantity
 
     def print_item_cost(self):
-        print("{} {} @ ${:.0f} = ${:.0f}".format(self.name, self.item_quantity, self.item_price, self.item_price * self.item_quantity))
+        print("{} {} @ ${:.0f} = ${:.0f}".format(self.item_name, self.item_quantity, self.item_price, self.item_price * self.item_quantity))
 
     def compute_total(self):
         return self.item_price * self.item_quantity
