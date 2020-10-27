ebe77ed9d7278b82f5ba66f42cd18090753b1578
Andrew <andrew.li729@gmail.com>
2020-10-27 16:09:50 -0500

    default constructor for ItemToPurchase class created

--- Lab_10.17.py
+++ Lab_10.17.py
@@ -1,3 +1,9 @@
 # Andrew Li
 # 1824794
 
+class ItemToPurchase:
+    def __init__(self, name = "none", item_price=0, item_quantity=0):
+        self.name = name
+        self.item_price = item_price
+        self.item_quantity = item_quantity
+
