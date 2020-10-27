58213d1a0abcf2907567ab892a32abf52c0f63ab
Andrew <andrew.li729@gmail.com>
2020-10-27 14:17:41 -0500

    initial commit

---
+++ Lab_10.11.py
@@ -0,0 +1,31 @@
+# Andrew Li
+# 1824794
+class FoodItem:
+    def __init__(self, name=None, fat=0, carbs=0, protein=0):
+        self.name = name
+        self.fat = fat
+        self.carbs = carbs
+        self.protein = protein
+
+    def get_calories(self, num_servings):
+        # Calorie formula
+        calories = ((self.fat * 9) + (self.carbs * 4) + (self.protein * 4)) * num_servings;
+        return calories
+
+    def print_info(self):
+        print('Nutritional information per serving of {}:'.format(self.name))
+        print('   Fat: {:.2f} g'.format(self.fat))
+        print('   Carbohydrates: {:.2f} g'.format(self.carbs))
+        print('   Protein: {:.2f} g'.format(self.protein))
+
+
+name = input()
+fat = int(input())
+carbs = int(input())
+protein = int(input())
+num_servings = int(input())
+
+food = FoodItem(name, fat, carbs, protein)
+total_calories = food.get_calories(num_servings)
+food.print_info()
+print(total_calories)
