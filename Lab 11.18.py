5a6127bdc8d4ae764c5c6ecde1123736b6dd78e7
Andrew <andrew.li729@gmail.com>
2020-10-30 16:09:44 -0500

    commented out debugging code

--- Lab 11.18.py
+++ Lab 11.18.py
@@ -5,7 +5,7 @@ input_list = input().split(" ")
 for i in range(0, len(input_list)):
     input_list[i] = int(input_list[i])
 input_list.sort()
-print(input_list)
+# print(input_list)  # for debugging
 
 for num in input_list:
     if num >= 0:
