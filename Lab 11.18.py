ef4d16c50ec0cae09815d4374afdfdbb3662151b
Andrew <andrew.li729@gmail.com>
2020-10-30 16:08:28 -0500

    setup input list and conversion from string to int

--- Lab 11.18.py
+++ Lab 11.18.py
@@ -1,2 +1,12 @@
 # Andrew Li
 # 1824794
+
+input_list = input().split(" ")
+for i in range(0, len(input_list)):
+    input_list[i] = int(input_list[i])
+input_list.sort()
+print(input_list)
+
+for num in input_list:
+    if num >= 0:
+        print(num, end=' ')
