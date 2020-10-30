bceb482cb3e20324b3a407703eea922631daf098
Andrew <andrew.li729@gmail.com>
2020-10-30 16:59:42 -0500

    completed player_dict user input

--- Lab_11.27.py
+++ Lab_11.27.py
@@ -4,4 +4,7 @@
 player_dict = {}
 
 for i in range(1, 6):
-    print(i)
+    jersey_num = input("Enter player {}'s jersey number:\n".format(i))
+    player_rating = input("Enter player {}'s rating:\n".format(i))
+    player_dict[jersey_num] = player_rating
+print(player_dict)
