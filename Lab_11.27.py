50cdb11e1bc81ed5e4246b951658d02c584a78c3
Andrew <andrew.li729@gmail.com>
2020-10-30 17:08:19 -0500

    implemented sorting by jersey number and printing

--- Lab_11.27.py
+++ Lab_11.27.py
@@ -2,9 +2,19 @@
 # 1824794
 
 player_dict = {}
+key_list = []
+option = ''
 
 for i in range(1, 6):
     jersey_num = input("Enter player {}'s jersey number:\n".format(i))
     player_rating = input("Enter player {}'s rating:\n".format(i))
     player_dict[jersey_num] = player_rating
-print(player_dict)
+# print(player_dict)
+
+for key in player_dict:
+    key_list.append(key)
+key_list.sort()
+
+for key in key_list:
+    print("Jersey number: {}, Rating: {}".format(key, player_dict[key]))
+
