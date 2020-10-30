9ca920fdcf0b088befa9058423c8fb619d268561
Andrew <andrew.li729@gmail.com>
2020-10-30 17:41:19 -0500

    fixed key_list sorting

--- Lab_11.27.py
+++ Lab_11.27.py
@@ -5,18 +5,23 @@ player_dict = {}
 key_list = []
 option = ''
 
+# gets user input and adds to dictionary
 for i in range(1, 6):
     jersey_num = input("Enter player {}'s jersey number:\n".format(i))
     player_rating = input("Enter player {}'s rating:\n".format(i))
+    print()
     player_dict[jersey_num] = player_rating
 # print(player_dict)
 
+# adds dict keys to list to be able to be sorted
 for key in player_dict:
     key_list.append(key)
-key_list.sort()
-print()
-print("ROSTER")
 
+key_list.sort(key=int)
+print(key_list)
+
+# prints roster created from user inputs
+print("\nROSTER")
 for key in key_list:
     print("Jersey number: {}, Rating: {}".format(key, player_dict[key]))
 
